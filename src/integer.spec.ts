77a0c2cec20efa3b64605aa2de44096553d6c603
Charles "Demurgos" Samborski <demurgos@demurgos.net>
2016-08-28 22:05:06 +0200

    Implement integer type

--- src/integer.spec.ts
+++ src/integer.spec.ts
@@ -1,46 +1,45 @@
-// import {IntegerTypeSync, IntegerType} from "./integer";
-// import {RunTestItem, runTestSync} from "./helpers/test";
-//
-// interface NumberConstructorES6 extends NumberConstructor{
-//   MAX_SAFE_INTEGER: number;
-//   MIN_SAFE_INTEGER: number;
-//   EPSILON: number;
-// }
-//
-// describe("NumberType", function () {
-//
-//   let type: IntegerTypeSync = new IntegerTypeSync();
-//
-//   let truthyItems: RunTestItem[] = [
-//     {value: 0, message: null},
-//     {value: 1, message: null},
-//     {value: -1, message: null},
-//     {value: 2, message: null},
-//     {value: 1e3, message: null},
-//     {value: -1e3, message: null},
-//     {value: (<NumberConstructorES6> Number).MAX_SAFE_INTEGER, message: null},
-//     {value: (<NumberConstructorES6> Number).MIN_SAFE_INTEGER, message: null},
-//     {value: Number.MAX_VALUE, message: null}
-//   ];
-//
-//   runTestSync(type, truthyItems);
-//
-//   let falsyItems: RunTestItem[] = [
-//     {name: "new Number(1)", value: new Number(1), message: ""},
-//     {name: "0.5", value: 0.5, message: ""},
-//     {name: "0.0001", value: 0.0001, message: ""},
-//     {name: "Infinity", value: Infinity, message: ""},
-//     {name: "-Infinity", value: -Infinity, message: ""},
-//     {name: "NaN", value: NaN, message: ""},
-//     {name: "undefined", value: undefined, message: ""},
-//     {name: "null", value: null, message: ""},
-//     {name: "Number.EPSILON", value: (<NumberConstructorES6> Number).EPSILON, message: ""},
-//     {name: '"0"', value: "0", message: ""},
-//     {name: "[]", value: [], message: ""},
-//     {name: "{}", value: {}, message: ""},
-//     {name: "/regex/", value: /regex/, message: ""}
-//   ];
-//
-//   runTestSync(type, falsyItems);
-//
-// });
+import {IntegerType} from "./integer";
+import {TestItem, runTests} from "./helpers/test";
+
+interface NumberConstructorES6 extends NumberConstructor{
+  MAX_SAFE_INTEGER: number;
+  MIN_SAFE_INTEGER: number;
+  EPSILON: number;
+}
+
+describe("IntegerType", function () {
+
+  let type: IntegerType = new IntegerType();
+
+  let items: TestItem[] = [
+    {name: "0", value: 0, valid: true},
+    {name: "1", value: 1, valid: true},
+    {name: "-1", value: -1, valid: true},
+    {name: "2", value: 2, valid: true},
+    {name: "1e3", value: 1e3, valid: true},
+    {name: "-1e3", value: 1e3, valid: true},
+    {name: "Number.MAX_SAFE_INTEGER", value: (<NumberConstructorES6> Number).MAX_SAFE_INTEGER, valid: true},
+    {name: "Number.MIN_SAFE_INTEGER", value: (<NumberConstructorES6> Number).MIN_SAFE_INTEGER, valid: true},
+    {name: "Number.MAX_VALUE", value: Number.MAX_VALUE, valid: true},
+
+    {name: "0.5", value: 0.5, valid: false},
+    {name: "0.0001", value: 0.0001, valid: false},
+    {name: "Number.EPSILON", value: (<NumberConstructorES6> Number).EPSILON, valid: false},
+    {name: 'new Number(true)', value: new Number(1), valid: false},
+    {name: '""', value: "", valid: false},
+    {name: '"0"', value: "0", valid: false},
+    {name: "Infinity", value: Infinity, valid: false},
+    {name: "-Infinity", value: -Infinity, valid: false},
+    {name: "NaN", value: NaN, valid: false},
+    {name: '"true"', value: "true", valid: false},
+    {name: '"false"', value: "false", valid: false},
+    {name: "undefined", value: undefined, valid: false},
+    {name: "null", value: null, valid: false},
+    {name: "[]", value: [], valid: false},
+    {name: "{}", value: {}, valid: false},
+    {name: "new Date()", value: new Date(), valid: false},
+    {name: "/regex/", value: /regex/, valid: false}
+  ];
+
+  runTests(type, items);
+});
--- src/integer.ts
+++ src/integer.ts
@@ -1,102 +1,134 @@
-// import {UnexpectedTypeError, ViaTypeError} from "./helpers/via-type-error";
-// import * as Bluebird from "bluebird";
-// import {TypeSync, TypeAsync} from "./interfaces";
-//
-// import * as numberType from "./number";
-// import {NumberType} from "./number";
-//
-// const NAME = "integer";
-//
-// export interface NumberOptions {}
-//
-//
-// function diffSync (oldVal: number, newVal: number, options?: NumberOptions): number | null {
-//   return oldVal === newVal ? null : newVal - oldVal;
-// }
-//
-// function patchSync (oldVal: number, diff: number | null, options?: NumberOptions): number {
-//   return diff === null ? oldVal : oldVal + diff;
-// }
-//
-// function reverseDiffSync (diff: number | null): number | null {
-//   return diff === null ? null : -diff;
-// }
-//
-// export class IntegerType implements
-//   TypeSync<number, number, NumberOptions>,
-//   TypeAsync<number, number, NumberOptions> {
-//
-//   isSync = true;
-//   isAsync = true;
-//   isCollection = true;
-//   type = NAME;
-//   types = [NAME];
-//
-//   numberType: NumberType = null;
-//
-//   constructor () {
-//     this.numberType = new NumberType();
-//   }
-//
-//   toJSON(): null {  // TODO: return options
-//     return null;
-//   }
-//
-//   readSync (format: "json-doc" | "bson-doc", val: any, options?: NumberOptions): number {
-//     const numVal = super.readSync(format, val, options);
-//     if (Math.floor(numVal) !== numVal) {
-//       throw new Error("Not an integer");
-//     }
-//     return numVal;
-//   }
-//
-//   readAsync (format: "json-doc" | "bson-doc", val: any, options?: NumberOptions): Bluebird<number> {
-//     return Bluebird.try(() => this.readSync(format, val, options));
-//   }
-//
-//   testErrorSync (val: any, options?: NumberOptions): Error | null {
-//     let err = super.testErrorSync(val, options);
-//     if (err === null && Math.floor(val) !== val) {
-//       err = new Error("Not an integer");
-//     }
-//     return err;
-//   }
-//
-//   testErrorAsync (val: any, options?: NumberOptions): Bluebird<Error | null> {
-//     return Bluebird.try(() => this.testErrorSync(val, options));
-//   }
-//
-//   testSync (val: any, options?: NumberOptions): boolean {
-//     return this.testErrorSync(val, options) === null;
-//   }
-//
-//   testAsync (val: any, options?: NumberOptions): Bluebird<boolean> {
-//     return Bluebird
-//       .try(() => this.testErrorAsync(val, options))
-//       .then((result) => result === null);
-//   }
-//
-//   diffSync (oldVal: number, newVal: number, options?: NumberOptions): number | null {
-//     return diffSync(oldVal, newVal, options);
-//   }
-//
-//   diffAsync (oldVal: number, newVal: number, options?: NumberOptions): Bluebird<number | null> {
-//     return Bluebird.try(() =>  diffSync(oldVal, newVal, options));
-//   }
-//
-//   patchSync (oldVal: number, diff: number | null, options?: NumberOptions): number {
-//     return patchSync(oldVal, diff, options);
-//   }
-//
-//   patchAsync (oldVal: number, diff: number | null, options?: NumberOptions): Bluebird<number> {
-//     return Bluebird.try(() => patchSync(oldVal, diff, options));
-//   }
-// }
-//
-// export class NumberTypeError extends ViaTypeError {}
-//
-// export class NumericError extends NumberTypeError {
-//   constructor (value: number) {
-//     super (null, "NumericError", {value: value}, "Value is not a number")
-//   }
-// }
+import {UnexpectedTypeError, ViaTypeError} from "./helpers/via-type-error";
+import * as Bluebird from "bluebird";
+import {TypeSync, TypeAsync} from "./interfaces";
+
+import * as numberType from "./number";
+import {NumberType} from "./number";
+
+const NAME = "integer";
+
+export interface NumberOptions {}
+
+
+function diffSync (oldVal: number, newVal: number): number | null {
+  return oldVal === newVal ? null : newVal - oldVal;
+}
+
+function patchSync (oldVal: number, diff: number | null): number {
+  return diff === null ? oldVal : oldVal + diff;
+}
+
+function reverseDiffSync (diff: number | null): number | null {
+  return diff === null ? null : -diff;
+}
+
+export class IntegerType implements
+  TypeSync<number, number, NumberOptions>,
+  TypeAsync<number, number, NumberOptions> {
+
+  isSync = true;
+  isAsync = true;
+  isCollection = true;
+  type = NAME;
+  types = [NAME];
+
+  numberType: NumberType = null;
+
+  constructor () {
+    this.numberType = new NumberType();
+  }
+
+  toJSON(): null {  // TODO: return options
+    return null;
+  }
+
+  readTrustedSync (format: "json-doc" | "bson-doc", val: any): number {
+    return val;
+  }
+
+  readTrustedAsync (format: "json-doc" | "bson-doc", val: any): Bluebird<number> {
+      return Bluebird.resolve(val);
+  }
+
+  readSync (format: "json-doc" | "bson-doc", val: any): number {
+    const numVal = this.numberType.readSync(format, val);
+    if (Math.floor(numVal) !== numVal) {
+      throw new Error("Not an integer");
+    }
+    return numVal;
+  }
+
+  readAsync (format: "json-doc" | "bson-doc", val: any): Bluebird<number> {
+    return Bluebird.try(() => this.readSync(format, val));
+  }
+
+  writeSync (format: "json-doc" | "bson-doc", val: number): any {
+    return val;
+  }
+
+  writeAsync (format: "json-doc" | "bson-doc", val: number): Bluebird<any> {
+    return Bluebird.resolve(val);
+  }
+
+  testErrorSync (val: any): Error | null {
+    let err = this.numberType.testErrorSync(val);
+    if (err === null && Math.floor(val) !== val) {
+      err = new Error("Not an integer");
+    }
+    return err;
+  }
+
+  testErrorAsync (val: any): Bluebird<Error | null> {
+    return Bluebird.try(() => this.testErrorSync(val));
+  }
+
+  testSync (val: any): boolean {
+    return this.testErrorSync(val) === null;
+  }
+
+  testAsync (val: any): Bluebird<boolean> {
+    return Bluebird
+      .try(() => this.testErrorAsync(val))
+      .then((result) => result === null);
+  }
+
+  equalsSync (val1: number, val2: number): boolean {
+    return val1 === val2;
+  }
+
+  equalsAsync (val1: number, val2: number): Bluebird<boolean> {
+    return Bluebird.resolve(val1 === val2);
+  }
+
+  cloneSync (val: number): number {
+    return val;
+  }
+
+  cloneAsync (val: number): Bluebird<number> {
+    return Bluebird.resolve(val);
+  }
+
+  diffSync (oldVal: number, newVal: number): number | null {
+    return diffSync(oldVal, newVal);
+  }
+
+  diffAsync (oldVal: number, newVal: number): Bluebird<number | null> {
+    return Bluebird.try(() =>  diffSync(oldVal, newVal));
+  }
+
+  patchSync (oldVal: number, diff: number | null): number {
+    return patchSync(oldVal, diff);
+  }
+
+  patchAsync (oldVal: number, diff: number | null): Bluebird<number> {
+    return Bluebird.try(() => patchSync(oldVal, diff));
+  }
+}
+
+export class NumberTypeError extends ViaTypeError {}
+
+export class NumericError extends NumberTypeError {
+  constructor (value: number) {
+    super (null, "NumericError", {value: value}, "Value is not a number")
+  }
+}
